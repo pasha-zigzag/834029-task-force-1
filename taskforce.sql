caa78d58afafbe3393738862e6b58f2460940a8a
Pavel.Zagainov <pasha_zigzag@mail.ru>
2020-12-07 23:54:40 +1000

    correction database

--- taskforce.sql
+++ taskforce.sql
@@ -1,125 +1,107 @@
 CREATE TABLE `task` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` varchar(255) NOT NULL,
-	`description` varchar(10000) NOT NULL,
-	`price` INT(11) NOT NULL,
-	`category_id` INT(11) NOT NULL,
-	`create_time` DATETIME NOT NULL,
-	`status` varchar(50) NOT NULL,
-	`city_id` INT(11) NOT NULL,
-	`lat` varchar(10) NOT NULL,
-	`long` varchar(10) NOT NULL,
-	`deadline` DATETIME NOT NULL,
-	`rating` INT(1) NOT NULL,
-	`file` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` TEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `price` int DEFAULT NULL,
+  `category_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `finish_at` datetime DEFAULT NULL,
+  `status` enum('new','canceled','in_work','performed','failed','completed') NOT NULL DEFAULT 'new',
+  `latitude` decimal(8,6) DEFAULT NULL,
+  `longitude` decimal(8,6) DEFAULT NULL,
+  `city_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `category` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` varchar(50) NOT NULL,
-	`icon` varchar(50) NOT NULL,
-	`price_color` varchar(10) NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` TEXT NOT NULL,
+  `code` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `user` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`password_hash` varchar(50) NOT NULL,
-	`register_date` DATETIME NOT NULL,
-	`city_id` INT(11) NOT NULL,
-	`avatar` varchar(50) NOT NULL,
-	`role` varchar(30) NOT NULL,
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` TEXT NOT NULL,
+	`email` TEXT NOT NULL,
+	`password_hash` text NOT NULL,
+	`register_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`city_id` INT NOT NULL,
+	`avatar` TEXT DEFAULT NULL,
+	`role` enum('customer','worker') NOT NULL DEFAULT 'customer',
 	`birthday` DATETIME NOT NULL,
-	`portfolio_photo` varchar(1000) NOT NULL,
-	`phone` varchar(11) NOT NULL,
-	`skype` varchar(50) NOT NULL,
-	`telegram` varchar(50) NOT NULL,
-	`show_profile` BOOLEAN NOT NULL,
-	`show_contacts` BOOLEAN NOT NULL,
-	`notification` varchar(255) NOT NULL,
+	`phone` TEXT DEFAULT NULL,
+	`skype` TEXT DEFAULT NULL,
+	`telegram` TEXT DEFAULT NULL,
+	`is_show_profile` tinyint NOT NULL DEFAULT '0',
+    `is_show_contacts` tinyint NOT NULL DEFAULT '0',
+    `is_notify_about_message` tinyint NOT NULL DEFAULT '0',
+    `is_notify_about_action` tinyint NOT NULL DEFAULT '0',
+    `is_notify_about_review` tinyint NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `task_response` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`task_id` INT(11) NOT NULL,
-	`worker_id` INT(11) NOT NULL,
-	`text` varchar(1000) NOT NULL,
-	`price` INT(11) NOT NULL,
-	`date` DATETIME NOT NULL,
+CREATE TABLE `response` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`task_id` INT NOT NULL,
+	`worker_id` INT NOT NULL,
+	`comment` TEXT NOT NULL,
+	`price` INT NOT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `city` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`lat` varchar(10) NOT NULL,
-	`long` varchar(10) NOT NULL,
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` TEXT NOT NULL,
+	`latitude` DECIMAL(8,6) DEFAULT NULL,
+	`longitude` DECIMAL(8,6) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `message` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`text` varchar(5000) NOT NULL,
-	`task_id` INT(11) NOT NULL,
-	`user_id` INT(11) NOT NULL,
-	`date` DATETIME NOT NULL,
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`task_id` INT NOT NULL,
+	`user_id` INT NOT NULL,
+	`text` TEXT NOT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `review` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`task_id` INT(11) NOT NULL,
-	`customer_id` INT(11) NOT NULL,
-	`worker_id` INT(11) NOT NULL,
-	`text` varchar(1000) NOT NULL,
-	`rating` INT(1) NOT NULL,
-	`date` DATETIME NOT NULL,
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`task_id` INT NOT NULL,
+	`customer_id` INT NOT NULL,
+	`worker_id` INT NOT NULL,
+	`comment` TEXT NOT NULL,
+	`rating` INT(1) DEFAULT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `user_category` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_id` INT(11) NOT NULL,
-	`category_id` INT(11) NOT NULL,
+	`user_id` INT NOT NULL,
+	`category_id` INT NOT NULL,
+	PRIMARY KEY (`user_id`,`category_id`)
+);
+
+CREATE TABLE `favorite` (
+	`customer_id` INT NOT NULL,
+	`worker_id` INT NOT NULL,
+	PRIMARY KEY (`customer_id`,`worker_id`)
+);
+
+CREATE TABLE `file` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`task_id` INT NOT NULL,
+	`name` TEXT NOT NULL,
+	`source` TEXT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `favorive` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`customer_id` INT(11) NOT NULL,
-	`worker_id` INT(11) NOT NULL,
+CREATE TABLE `portfolio` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`user_id` INT NOT NULL,
+	`source` TEXT NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
-ALTER TABLE `task` ADD CONSTRAINT `task_fk0` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`);
-
-ALTER TABLE `task` ADD CONSTRAINT `task_fk1` FOREIGN KEY (`city_id`) REFERENCES `city`(`id`);
-
-ALTER TABLE `user` ADD CONSTRAINT `user_fk0` FOREIGN KEY (`city_id`) REFERENCES `city`(`id`);
-
-ALTER TABLE `task_response` ADD CONSTRAINT `task_response_fk0` FOREIGN KEY (`task_id`) REFERENCES `task`(`id`);
-
-ALTER TABLE `task_response` ADD CONSTRAINT `task_response_fk1` FOREIGN KEY (`worker_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `message` ADD CONSTRAINT `message_fk0` FOREIGN KEY (`task_id`) REFERENCES `task`(`id`);
-
-ALTER TABLE `message` ADD CONSTRAINT `message_fk1` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `review` ADD CONSTRAINT `review_fk0` FOREIGN KEY (`task_id`) REFERENCES `task`(`id`);
-
-ALTER TABLE `review` ADD CONSTRAINT `review_fk1` FOREIGN KEY (`customer_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `review` ADD CONSTRAINT `review_fk2` FOREIGN KEY (`worker_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `user_category` ADD CONSTRAINT `user_category_fk0` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `user_category` ADD CONSTRAINT `user_category_fk1` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`);
-
-ALTER TABLE `favorive` ADD CONSTRAINT `favorive_fk0` FOREIGN KEY (`customer_id`) REFERENCES `user`(`id`);
-
-ALTER TABLE `favorive` ADD CONSTRAINT `favorive_fk1` FOREIGN KEY (`worker_id`) REFERENCES `user`(`id`);
-
